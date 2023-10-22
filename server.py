6e5bff85cd3945b128a2886ac7057ef799717f29
Arman Moztarzadeh <armanmoztar@outlook.com>
2023-10-21 17:35:42 -0700

    Implement flask to remove CORS error

--- server.py
+++ server.py
@@ -10,6 +10,11 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense
 import tensorflow as tf
 from keras import backend as K
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
 
 print(K.backend())
 
